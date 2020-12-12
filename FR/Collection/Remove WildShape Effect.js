eb8923830d90dae10d691f8a34232caac0b49e0e
MisterHims <mister.hims@gmail.com>
2020-12-12 23:00:22 +0100

    update

--- FR/Collection/Remove WildShape Effect.js
+++
@@ -1,5 +0,0 @@
-setTimeout(function () {
-    let WildShapeEffect = game.actors.getName(args[0]);
-    let removeWildShapeEffect = WildShapeEffect.effects.find(i => i.data.label === args[1]);
-    removeWildShapeEffect.delete();
-}, 3500);
